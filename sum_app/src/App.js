a74742cafc748c540f04eb6993b7883c882b4275
Gustavo Duarte Gomes <gu_gugu15@hotmail.com>
2021-06-06 20:05:31 -0300

    Sum_App

---
+++ sum_app/src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Some dois numeros inteiros !
+        </p>
+        <div className="Sum">
+          <p className="Info">Insira o primeiro valor:</p>
+          <input type="text" id = "n1"/><br/>
+          <p className="Info">Insira o segundo valor:</p>
+          <input type="text" id = "n2"/><br/>
+          <p className="Info">Informe o operador:</p>
+          <select id="operators">
+            <option value="+">+</option>
+          </select><br/>
+
+          <p className="Info">Resultado:</p>
+          <button onClick={calc}>=</button>
+          <input type="text" id = "result"></input>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+function calc() {
+  var n1 = parseInt(document.getElementById('n1').value);
+  var n2 = parseInt(document.getElementById('n2').value);
+  var oper = document.getElementById('operators').value;
+
+  if(oper === '+')
+  {
+    document.getElementById('result').value = n1+n2;
+  }
+}
+
+export default App;
